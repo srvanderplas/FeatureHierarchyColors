5c93033e75fda3710e6658f3976212dc6ef4a831
Susan VanderPlas <srvanderplas@gmail.com>
2015-09-12 17:13:31 -0500

    Code for different color groups

--- Code/ReassignGroups.R
+++ Code/ReassignGroups.R
@@ -21,7 +21,7 @@ data <- filelist %>%
   rowwise() %>%
   do({
     pars <- extract.data.pars(.$filename)
-    data.frame(pars, read.csv(file = .$filename, stringsAsFactors=FALSE), stringsAsFactors=FALSE)
+    data.frame(pars, read.csv(file = .$filename, stringsAsFactors = FALSE), stringsAsFactors = FALSE)
   }) %>% ungroup() %>%
   group_by(set) %>%
   do({
@@ -32,3 +32,34 @@ data <- filelist %>%
   })
 
 save(data, file = "Images/DataWithNewGroups.RData")
+
+source("Code/Turk16Palette.R")
+library(RColorBrewer)
+library(dichromat)
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+paired.to.categ <- c(7, 8, 5, 6, 11, 12, 1, 2, 9, 10, 3, 4)
+
+data.frame(
+  type = c(
+    rep("ggplot", 8),
+    rep("turk16", 8),
+    rep("tableau", 8),
+    rep("colorbrewer", 8),
+    rep("dichromat", 8)
+  ),
+  N = rep(
+    c(rep(3, 3), rep(5, 5))
+    times = 6
+  ),
+  color = c(
+    gg_color_hue(3), gg_color_hue(5),
+    best.combo(3, colors, colortm), best.combo(5, colors, colortm),
+    colors[1:3], colors[1:5],
+    brewer.pal(3, "Paired"), brewer.pal(5, "Paired")
+  )
+)
---
+++ Code/Turk16Palette.R
@@ -0,0 +1,39 @@
+best.combo <- function(ngroups=3, palette, dist.matrix){
+  # check distance matrix
+  if(nrow(dist.matrix)!=length(palette) | ncol(dist.matrix)!=length(palette)){
+    stop(paste0("The distance matrix does not match the size of the palette. ",
+                "It should be ", length(palette), "x", length(palette), "."))
+  }
+  if(sum(dist.matrix<0)>0){
+    stop("Distance matrix cannot have negative entries.")
+  }
+
+  require(combinat)
+  clist <- t(combn(1:length(palette), ngroups))
+  pairwise.combos <- t(combn(1:ngroups, 2))
+  res <- rowSums(apply(pairwise.combos, 1, function(i){diag(as.matrix(dist.matrix[clist[,i[1]], clist[,i[2]]]))}))
+
+  return(palette[clist[which.max(res),]])
+}
+
+
+# Define colors and shapes
+colors <-  c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+             "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf")
+
+shapes <- c(1,0,3,4,8,5,2,6,-0x25C1, -0x25B7)
+
+colortm <- read.csv("./Data/color-perceptual-kernel.csv")
+# colortm[3,4] <- 0
+# colortm[4,3] <- 0
+colortm[8,] <- 0
+colortm[,8] <- 0
+
+shapetm <- read.csv("./Data/shape-perceptual-kernel.csv")
+# shapetm[9:10,] <- 0
+# shapetm[, 9:10] <- 0
+shapetm[9,] <- 0
+shapetm[,9] <- 0
+shapetm[10,] <- 0
+shapetm[,10] <- 0
+
